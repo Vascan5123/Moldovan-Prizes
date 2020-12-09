97407d510ef428f51762fe5dc1ecf05ae4ed52d5
Lupan Vasile <66137225+Vascan5123@users.noreply.github.com>
2020-12-09 18:55:45 +0300

    1

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDVWRgnboXXZv8uNz1JDF1L5ay8oOm8Mac",
+    authDomain: "moldovan-prizes-in-logic.firebaseapp.com",
+    projectId: "moldovan-prizes-in-logic",
+    storageBucket: "moldovan-prizes-in-logic.appspot.com",
+    messagingSenderId: "115986320501",
+    appId: "1:115986320501:web:1bf49cf3612e302fcb3c3c"
+  }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import firebaseConfig from "./config/firebase.js"
+import firebase from 'firebase/app'
+import "firebase/firestore"
+import 'firebase/auth'
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore()
+
+Vue.$db = db
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
+const vm = new Vue({
+  vuetify,
+  beforeCreate() {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        vm.$store.dispatch("stateChange", user)
+      } else {
+        // No user is signed in.
+      }
+    });
+  },
+  render: h => h(App)
 }).$mount('#app')
